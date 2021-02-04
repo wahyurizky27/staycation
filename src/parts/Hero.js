449101be1799dda0f674fcc7e9cbea2985e67add
Wahyu Rizky Ramadhan <64141752+wahyurizky27@users.noreply.github.com>
2021-02-04 20:52:46 +0700

    update hero description

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -22,8 +22,8 @@ export default function Hero(props) {
             <div className="row align-items-center">
                 <div className="col-auto pr-5" style={{ width: 530 }}>
                     <h1 className="font-weight-bold line-height-1 mb-3">
-                        Forget Busy Work, <br/>
-                        Start Next Vacation
+                        Forget Busy work and Covid, <br/>
+                        Start Next Vacation !
                     </h1>
                     <p className="mb-4 font-weight-light text-gray-500 w-75" style={{lineHeight:"170%"}}>
                     We provide what you need to enjoy your 
